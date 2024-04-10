7ffaf36391f974bc2a39dcb48df08cd8340d03c5
Mauri <mauri.monzon91@gmail.com>
2024-04-10 10:33:16 -0300

    Peticion desde el front

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,16 @@
-export default function RestTodoPage() {
+import prisma from "@/app/lib/prisma";
+
+export const metadata = {
+  title: "Listado de TODOS",
+  description: "SEO Title",
+};
+
+export default async function RestTodoPage() {
+  const todos = await prisma.todo.findMany();
   return (
     <div>
       <h1>Page RestTodo</h1>
+      {JSON.stringify(todos)}
     </div>
   );
 }

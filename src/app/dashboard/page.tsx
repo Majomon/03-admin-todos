685cdec0246163152928880dedd4b2a527450cef
Mauri <mauri.monzon91@gmail.com>
2024-04-11 12:20:14 -0300

    Login con github - Hecho

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,20 @@
 import { WidgetItem } from "@/components";
-
+import { auth } from "../../auth";
 // prc
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await auth();
+
+  if (!session?.user) return null;
+
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem />
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+      <WidgetItem title="Usuario conectado S-Side">
+        <div className="flex flex-col">
+          <span>{session.user?.name}</span>
+          <span>{session.user?.image}</span>
+          <span>{session.user?.email}</span>
+        </div>
+      </WidgetItem>
     </div>
   );
 }

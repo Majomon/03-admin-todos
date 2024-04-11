478a71b39e6e88c469e5a913b5f15ba713a02808
Mauri <mauri.monzon91@gmail.com>
2024-04-11 10:58:34 -0300

    act

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { WidgetItem } from "@/components";
 
 export default function Home() {
   return (
-   <>
-   <span className="text-5xl">Hola mundo</span>
-   </>
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      {/* <WidgetItem /> */}
+    </div>
   );
 }

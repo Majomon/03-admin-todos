68d465637d15ad3fe7c5051d45ef9a54262366a1
Mauri <mauri.monzon91@gmail.com>
2024-04-11 11:48:13 -0300

    Autentificacion con github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { WidgetItem } from "@/components";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/dashboard");
+
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
       {/* <WidgetItem /> */}
